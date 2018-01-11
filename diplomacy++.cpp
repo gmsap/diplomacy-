e71fac8734ccb2088030582f3be9e09f863b12fb
sam_alvis <alvibry@yahoo.com>
2018-01-11 09:42:06 -0600

    set up tiles of the board slightly better, more work needed

--- diplomacy++.cpp
+++ diplomacy++.cpp
@@ -21,87 +21,88 @@ public:
 		this->holdsArmy = true;
 		this->unitType = ' ';			//'A' is army, 'F' is fleet, ' ' is empty
 		this->unitOwner = ' ';			//character for the country
-		// must have some way of initializing neighbors
+		//must have some way of initializing neighbors
 	}
 };
 
 struct board
 {
 public:
-	tile NAO; //water
-	tile NWG;
-	tile BAR;
-	tile NTH;
-	tile GOB;
-	tile IRE;
-	tile HEL;
-	tile SKA;
-	tile BAL;
-	tile MAO;
-	tile GOL;
-	tile ADR;
-	tile BLA;
-	tile WES;
-	tile TYR;
-	tile ION;
-	tile AEG;
-	tile EAS; //water
-	tile Cly; //england
-	tile Edi;
-	tile Lvp;
-	tile Yor;
-	tile Wal;
-	tile Lon; //england
-	tile Nor; //norse
-	tile Swe;
-	tile Den; //norse
-	tile Fin; //russia
-	tile Stp;
-	tile Liv;
-	tile Mos;
-	tile War;
-	tile Ukr;
-	tile Sev; //russia
-	tile Arm; //turkey
-	tile Syr;
-	tile Smy;
-	tile Ank;
-	tile Con; //turkey
-	tile Bul; //balkan
-	tile Gre;
-	tile Alb;
-	tile Ser;
-	tile Rum; //balkan
-	tile Gal; //austria-hungary
-	tile Boh;
-	tile Tyr;
-	tile Vie;
-	tile Bud;
-	tile Tri; //austria-hungary
-	tile Ven; //italy
-	tile Apu;
-	tile Nap;
-	tile Rom;
-	tile Tus;
-	tile Pie; //italy
-	tile Tun; //mediteranean
-	tile Naf;
-	tile Por;
-	tile Spa; //mediteranean
-	tile Mar; //france
-	tile Gas;
-	tile Bur;
-	tile Par;
-	tile Bre;
-	tile Pic; //france
-	tile Bel; //low countries
-	tile Hol; //low countries
-	tile Ruh; //germany
-	tile Mun;
-	tile Sil;
-	tile Pru;
-	tile Ber;
-	tile Kie; //germany
+	tile* tiles[74];
+	tiles[0] = new tile("NAO"); //water
+	tiles[1] = new tile("NWG");
+	tiles[2] = new tile("BAR");
+	tiles[3] = new tile("NTH");
+	tiles[4] = new tile("GOB");
+	tiles[5] = new tile("IRE");
+	tiles[6] = new tile("HEL");
+	tiles[7] = new tile("SKA");
+	tiles[8] = new tile("BAL");
+	tiles[9] = new tile("MAO");
+	tiles[10] = new tile("GOL");
+	tiles[11] = new tile("ADR");
+	tiles[12] = new tile("BLA");
+	tiles[13] = new tile("WES");
+	tiles[14] = new tile("TYR");
+	tiles[15] = new tile("ION");
+	tiles[16] = new tile("AEG");
+	tiles[17] = new tile("EAS"); //water
+	tiles[18] = new tile("Cly"); //england
+	tiles[19] = new tile("Edi");
+	tiles[20] = new tile("Lvp");
+	tiles[21] = new tile("Yor");
+	tiles[22] = new tile("Wal");
+	tiles[23] = new tile("Lon"); //england
+	tiles[24] = new tile("Nor"); //norse
+	tiles[25] = new tile("Swe");
+	tiles[26] = new tile("Den"); //norse
+	tiles[27] = new tile("Fin"); //russia
+	tiles[28] = new tile("Stp");
+	tiles[29] = new tile("Liv");
+	tiles[30] = new tile("Mos");
+	tiles[31] = new tile("War");
+	tiles[32] = new tile("Ukr");
+	tiles[33] = new tile("Sev"); //russia
+	tiles[34] = new tile("Arm"); //turkey
+	tiles[35] = new tile("Syr");
+	tiles[36] = new tile("Smy");
+	tiles[37] = new tile("Ank");
+	tiles[38] = new tile("Con"); //turkey
+	tiles[39] = new tile("Bul"); //balkan
+	tiles[40] = new tile("Gre");
+	tiles[41] = new tile("Alb");
+	tiles[42] = new tile("Ser");
+	tiles[43] = new tile("Rum"); //balkan
+	tiles[44] = new tile("Gal"); //austria-hungary
+	tiles[45] = new tile("Boh");
+	tiles[46] = new tile("Tyr");
+	tiles[47] = new tile("Vie");
+	tiles[48] = new tile("Bud");
+	tiles[49] = new tile("Tri"); //austria-hungary
+	tiles[50] = new tile("Ven"); //italy
+	tiles[51] = new tile("Apu");
+	tiles[52] = new tile("Nap");
+	tiles[53] = new tile("Rom");
+	tiles[54] = new tile("Tus");
+	tiles[55] = new tile("Pie"); //italy
+	tiles[56] = new tile("Tun"); //mediteranean
+	tiles[57] = new tile("Naf");
+	tiles[58] = new tile("Por");
+	tiles[59] = new tile("Spa"); //mediteranean
+	tiles[60] = new tile("Mar"); //france
+	tiles[61] = new tile("Gas");
+	tiles[62] = new tile("Bur");
+	tiles[63] = new tile("Par");
+	tiles[64] = new tile("Bre");
+	tiles[65] = new tile("Pic"); //france
+	tiles[66] = new tile("Bel"); //low countries
+	tiles[67] = new tile("Hol"); //low countries
+	tiles[68] = new tile("Ruh"); //germany
+	tiles[69] = new tile("Mun");
+	tiles[70] = new tile("Sil");
+	tiles[71] = new tile("Pru");
+	tiles[72] = new tile("Ber");
+	tiles[73] = new tile("Kie"); //germany
 
 	board(){
 		//sets up ocean and inland countries
