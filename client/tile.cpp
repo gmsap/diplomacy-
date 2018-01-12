f818b2044c16c1f4e6fe5da1011437107658753c
sam_alvis <alvibry@yahoo.com>
2018-01-12 13:25:17 -0600

    more changes to tile data structure (changes noted in comments)

--- client/tile.cpp
+++ client/tile.cpp
@@ -6,22 +6,20 @@ struct tile {
 public:
 	string name;
 	bool hasSupplyCenter;
-	bool holdsFleet;
+	bool holdsFleet; // bool holdsFleet and bool holdsArmy determine if a tile is landlocked, coastal, or ocean - no char tileType required
 	bool holdsArmy;
-	char owner; // first letter of the empire to most recently own the territory
-    char tileType; //'L' for landlocked, 'C' for coastal, 'S' for sea
+	char owner; // first letter of the empire to most recently own the territory NOTE: changed from string - there are only 7 possible chars: E, F, G, I, A, R, and T - no string required
 	tile** neighbors; //an array of pointers to tiles
 
-	tile(string name, bool hasSupplyCenter, bool holdsFleet, bool holdsArmy, string owner, char tileType, int numNeighbors, tile* neighbors) {
+	tile(string name, bool hasSupplyCenter, bool holdsFleet, bool holdsArmy, char owner, int numNeighbors, tile* neighbors) {
 		this->name = name;
 		this->hasSupplyCenter = hasSupplyCenter;
 		this->holdsFleet = holdsFleet;
 		this->holdsArmy = holdsArmy;
         this->owner = owner;
+
         for (int i = 0; i < numNeighbors; i++) {
             this->neighbors[i] = &neighbors[i];    
         }
     }
 };
-
-
